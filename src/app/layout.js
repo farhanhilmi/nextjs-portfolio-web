0bf573090678e1b58719fc7d06d596c5b2e71a99
Farhan Hilmi <farhanhilmi077@gmail.com>
2024-01-04 21:15:07 +0700

    fix: remove use client

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,7 @@
-'use client';
-
-import { useState } from 'react';
 import './globals.css';
-import { Inter } from 'next/font/google';
 import Head from 'next/head';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
     title: 'Farhan Hilmi',
@@ -15,35 +9,6 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-    const [timeout, setTimeOut] = useState(null);
-    const [style, setStyle] = useState({});
-
-    // const handleMouseMove = (e) => {
-    //     const x = e.pageX;
-    //     const y = e.pageY;
-    //     setStyle({
-    //         top: `${y}px`,
-    //         left: `${x}px`,
-    //         display: `block`,
-    //     });
-    //     children.params.props.style = style;
-
-    //     const handleMouseStop = () => {
-    //         setStyle({
-    //             display: `none`,
-    //         });
-    //         children.params.props.style = style;
-    //     };
-    //     clearTimeout(timeout);
-    //     setTimeOut(setTimeOut(handleMouseStop, 1000));
-    // };
-
-    // const handleMouseStop = () => {
-    //     setStyle({
-    //         display: `none`,
-    //     });
-    //     children.params.props.style = style;
-    // };
     return (
         <html lang="en">
             <Head>
@@ -52,14 +17,7 @@ export default function RootLayout({ children }) {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <body
-                // className="bg-[#134E5E] "
-                className="bg-gradient-to-r from-[#3A6073] to-[#16222A] font-inter"
-
-                // onMouseOut={handleMouseStop}
-                // onMouseMove={handleMouseMove}
-                // className={inter.className}
-            >
+            <body className="bg-gradient-to-r from-[#3A6073] to-[#16222A] font-inter">
                 <ToastContainer />
 
                 {children}
